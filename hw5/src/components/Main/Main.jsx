15aad513e2744feb4bf2af547e013d942d788548
Nikita <ist.nik10@gmail.com>
2025-05-14 16:53:38 +0200

    intermediate version

--- hw5/src/components/Main/Main.jsx
+++ hw5/src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import styles from "./Main.module.scss";
 import SearchBar from "../SearchBar/SearchBar";
 import CategoryCardList from "../CategoryCardList/CategoryCardList";
+import VacancyCardList from "../VacancyCardList/VacancyCardList";
 
 function Main() {
   return (
     <div>
       <SearchBar />
       <CategoryCardList />
+      <VacancyCardList />
     </div>
   );
 }
